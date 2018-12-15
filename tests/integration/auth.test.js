239d7b17ec702958dd3bf90c13fce4b8094def11
kaislearning <kaislearning7@gmail.com>
2018-12-15 11:55:37 +0700

    First commit

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const { Genre } = require('../../models/genre');
+const { User } = require('../../models/user');
+let server;
+
+describe('auth middleware', () => {
+    beforeEach(() => { server = require('../../index'); });
+    afterEach(async () => {
+        await server.close();
+        await Genre.remove({});
+    });
+    
+    let token;
+
+    const execute = () => {
+        return request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({ name: 'genre1' });
+    }
+    
+    beforeEach(() => { token = new User().generateAuthToken(); });
+
+	it('should return 401 if no token provided', async () => {
+        token = '';
+        const res = await execute();
+        expect(res.status).toBe(401);
+    });
+    
+    it('should return 400 if invalid token', async () => {
+        token = 'a';
+        const res = await execute();
+        expect(res.status).toBe(400);
+    });
+    
+    it('should return 200 if valid token', async () => {
+        const res = await execute();
+        expect(res.status).toBe(200);
+	});
+});
